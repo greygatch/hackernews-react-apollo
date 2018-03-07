19ebe45300559988ceb7c9cabbd8c4c417833e0d
Evan Gatchell <evan.gatchell@hcahealthcare.com>
2018-03-07 09:22:14 -0600

    POST mutation wired up.

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class CreateLink extends Component {
   state = {
@@ -31,8 +33,25 @@ class CreateLink extends Component {
   }
 
   _createLink = async () => {
-    // ... you'll implement this in a bit
+    const { description, url } = this.state
+    await this.props.postMutation({
+      variables: {
+        description,
+        url
+      }
+    })
   }
 }
 
-export default CreateLink
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+export default graphql(POST_MUTATION, { name: 'postMutation' })(CreateLink)
