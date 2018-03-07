2ae72095d01ea127a43f8a8a37bc87149eb0f5e1
Evan Gatchell <evan.gatchell@hcahealthcare.com>
2018-03-07 09:36:48 -0600

    add constants and proto login functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../styles/App.css';
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Header from './Header'
+import Login from './Login'
 import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
@@ -13,8 +14,9 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
-            <Route exact path="/" component={LinkList} />
+            <Route exact path="/login" component={Login} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/" component={LinkList} />
           </Switch>
         </div>
       </div>
