c414f6b5a58be1c9090cd4e28739a20c3ec69b68
Evan Gatchell <evan.gatchell@hcahealthcare.com>
2018-03-07 09:28:19 -0600

    added routing to client app

--- src/index.js
+++ src/index.js
@@ -3,26 +3,25 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-// 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
 
-// 2
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
-// 3
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 })
 
-// 4
 const JSXClient = (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
 );
 
 ReactDOM.render(JSXClient, document.getElementById('root'));
